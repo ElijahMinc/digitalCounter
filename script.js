7d12d37c16f6968c94f9e25517dc1a1f22c1659e
ElijahMinc <gvidar89@gmail.com>
2020-12-30 09:08:47 +0200

    addOneStringJsCode

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const counter = document.querySelector('.counter');
 
 function digitalCounter() {
    const areaValue = textarea.value;//содержимое текстареа
-   const areaLength = areaValue.length; // длина содержимого текст-ареа
+   const areaLength = areaValue.length; // длина ЗНАЧЕНИЯ СОДЕРЖИМОГО textarea
 
    counter.innerText = areaLength;// object.innerText - позволяет управлять текстовым содержимым.
    // возможно как вывести содержимое, так и установить его.
 }
-// const valueArea = textarea.innerText = 'Hello world';
-
-// console.log(valueArea);
+const valueArea = textarea.innerText = 'Здесь 17 символов';
